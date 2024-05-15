8861b422ca40a4b295b8a3d2b56d91a2b639132a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-15 11:52:54 -0700

    circular header

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -7,6 +7,6 @@ class HeadersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SquareHeader());
+    return const Scaffold(body: CircularHeader());
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -11,3 +11,19 @@ class SquareHeader extends StatelessWidget {
     );
   }
 }
+
+class CircularHeader extends StatelessWidget {
+  const CircularHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+          color: Color(0xff615aab),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(70),
+              bottomRight: Radius.circular(70))),
+    );
+  }
+}
