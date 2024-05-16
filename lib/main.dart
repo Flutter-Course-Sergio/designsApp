3caebcb50effb13618db28439c7a7ff256d5013b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-16 09:54:12 -0700

    circular progress page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-//import 'package:designs_app/src/pages/headers_page.dart';
-//import 'package:designs_app/src/pages/animations_page.dart';
-import 'src/challenges/square_animated_page.dart';
+//import 'src/pages/headers_page.dart';
+//import 'src/pages/animations_page.dart';
+//import 'src/challenges/square_animated_page.dart';
+import 'src/pages/circular_progress_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SquareAnimatedPage(),
+      home: CircularProgressPage(),
       title: 'Designs App',
       debugShowCheckedModeBanner: false,
     );
