bfd44676ffed56237f4fda204c2e1cf4da0fcbb4
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-20 18:01:30 -0700

    dots design

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -7,7 +7,13 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _Slides()),
+      body: Center(
+          child: Column(
+        children: [
+          Expanded(child: _Slides()),
+          const _Dots(),
+        ],
+      )),
     );
   }
 }
@@ -17,14 +23,12 @@ class SlideShowPage extends StatelessWidget {
 class _Slides extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PageView(
-        children: const [
-          _Slide('assets/svgs/slide-1.svg'),
-          _Slide('assets/svgs/slide-2.svg'),
-          _Slide('assets/svgs/slide-3.svg')
-        ],
-      ),
+    return PageView(
+      children: const [
+        _Slide('assets/svgs/slide-1.svg'),
+        _Slide('assets/svgs/slide-2.svg'),
+        _Slide('assets/svgs/slide-3.svg')
+      ],
     );
   }
 }
@@ -41,5 +45,36 @@ class _Slide extends StatelessWidget {
         height: double.infinity,
         padding: const EdgeInsets.all(30),
         child: SvgPicture.asset(svg));
+  }
+}
+
+class _Dots extends StatelessWidget {
+  const _Dots();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [_Dot(), _Dot(), _Dot()],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration:
+          const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+    );
   }
 }
