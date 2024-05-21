3121f9b3538fd483a6bb25b68405fa17029d1f2c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-20 18:42:44 -0700

    optimization start

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SlideShowPage(),
+      home: SlideshowPage(),
       title: 'Designs App',
       debugShowCheckedModeBanner: false,
     );
