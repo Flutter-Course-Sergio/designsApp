acdf8774db54ec78c5ccf67f408cb46db99c9b01
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-21 18:34:08 -0700

    fat button design

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../widgets/fat_button.dart';
 import '../widgets/headers.dart';
 
 class EmergencyPage extends StatelessWidget {
@@ -9,13 +10,26 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plus,
-        title: 'Haz solicitado',
-        subtitle: 'Asistencia Médica',
-        color1: Color(0xff526bf6),
-        color2: Color(0xff67acf2),
+      body: Center(
+        child: FatButton(),
       ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const IconHeader(
+      icon: FontAwesomeIcons.plus,
+      title: 'Haz solicitado',
+      subtitle: 'Asistencia Médica',
+      color1: Color(0xff526bf6),
+      color2: Color(0xff67acf2),
     );
   }
 }
---
+++ lib/src/widgets/fat_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FatButton extends StatelessWidget {
+  const FatButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _FatButtonBackground();
+  }
+}
+
+class _FatButtonBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                offset: const Offset(4, 6),
+                blurRadius: 10)
+          ],
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            Color(0xff6989f5),
+            Color(0xff906ef5),
+          ])),
+    );
+  }
+}
